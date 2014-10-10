1a8684c207b2671d23934211212e61dae8f7769a
baijiayong <jiayongbai@gmail.com>
2014-10-10 11:41:47 +0800

    添加联系人的list及其测试，并在页面中显示出联系人的信息

--- src/main/java/com/baldurtech/contact/ContactController.java
+++ src/main/java/com/baldurtech/contact/ContactController.java
@@ -6,13 +6,32 @@ import org.springframework.web.bind.annotation.RequestMethod;
 
 import org.springframework.ui.Model;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Controller
 @RequestMapping("/contact")
 public class ContactController {
         
     @RequestMapping(value = "/list", method = RequestMethod.GET)    
     public String contactList(Model model) {
-        model.addAttribute("contactList", "ContactList");
+        List<Contact> contactList = new ArrayList<Contact>();
+        Contact contact = new Contact();       
+        contact.setName("shihang");
+        contact.setMobile("15235432994");
+        contact.setHomeAddress("shanxi");
+        contact.setVpmn("652994");
+        
+        Contact contact2 = new Contact();
+        contact2.setName("xiaobai");
+        contact2.setMobile("18235433333");
+        contact2.setHomeAddress("taiyuan");
+        contact2.setVpmn("655555");
+        
+        contactList.add(contact);
+        contactList.add(contact2);
+        
+        model.addAttribute("contactList", contactList);
         return "contact/list";
     }
 }
--- src/test/java/com/baldurtech/contact/ContactControllerTest.java
+++ src/test/java/com/baldurtech/contact/ContactControllerTest.java
@@ -2,6 +2,8 @@ package com.baldurtech.contact;
 
 import org.junit.Test;
 
+import static org.hamcrest.Matchers.allOf;
+import static org.hamcrest.Matchers.containsString;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
@@ -12,6 +14,14 @@ public class ContactControllerTest extends WebAppConfigurationAware {
     public void displaysContactList() throws Exception {
         mockMvc.perform(get("/contact/list"))
                 .andExpect(view().name("contact/list"))
-                .andExpect(model().attributeExists("contactList"));
+                .andExpect(model().attributeExists("contactList"))
+                .andExpect(content().string(
+                        allOf(
+                                containsString("<title>contactList</title>"),
+                                containsString("<td>shihang</td>"),
+                                containsString("<td>15235432994</td>"),
+                                containsString("<td>652994</td>"),
+                                containsString("<td>shanxi</td>")
+                        )));
     }
 }
