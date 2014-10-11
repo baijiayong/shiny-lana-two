62494e01c780692239adef766f013a9c860d03eb
baijiayong <jiayongbai@gmail.com>
2014-10-11 14:13:56 +0800

    在ContactController中添加list列表

--- src/main/java/com/baldurtech/contact/ContactController.java
+++ src/main/java/com/baldurtech/contact/ContactController.java
@@ -16,6 +16,21 @@ public class ContactController {
     @RequestMapping(value = "/list", method = RequestMethod.GET)
     public String list(Model model) {
         List<Contact> list = new ArrayList<Contact>();
+        Contact contact = new Contact();       
+        contact.setName("shihang");
+        contact.setMobile("15235432994");
+        contact.setHomeAddress("shanxi");
+        contact.setVpmn("652994");
+        
+        Contact contact2 = new Contact();
+        contact2.setName("xiaobai");
+        contact2.setMobile("18235433333");
+        contact2.setHomeAddress("taiyuan");
+        contact2.setVpmn("655555");
+        
+        list.add(contact);
+        list.add(contact2);
+        
         model.addAttribute("list", list);
         return "contact/list";
     }
