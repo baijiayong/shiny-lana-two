65adcb4f34390ce62ccc52c5ee7957e8208269d0
baijiayong <jiayongbai@gmail.com>
2014-10-11 14:40:07 +0800

    添加了ContactService并在ContactController中自动装配，并且添加测试

--- src/main/java/com/baldurtech/contact/ContactController.java
+++ src/main/java/com/baldurtech/contact/ContactController.java
@@ -1,6 +1,7 @@
 package com.baldurtech.contact;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -12,31 +13,17 @@ import java.util.ArrayList;
 @Controller
 @RequestMapping("contact")
 public class ContactController {
+    ContactService contactService;
+    
+    @Autowired
+    public ContactController(ContactService contactService) {
+        this.contactService = contactService;
+    }
 
     @RequestMapping(value = "/list", method = RequestMethod.GET)
     public String list(Model model) {
        
-        model.addAttribute("list", getList());
+        model.addAttribute("list", contactService.getList());
         return "contact/list";
-    }
-    
-    public List<Contact> getList() {
-        List<Contact> list = new ArrayList<Contact>();
-        Contact contact = new Contact();       
-        contact.setName("shihang");
-        contact.setMobile("15235432994");
-        contact.setHomeAddress("shanxi");
-        contact.setVpmn("652994");
-        
-        Contact contact2 = new Contact();
-        contact2.setName("xiaobai");
-        contact2.setMobile("18235433333");
-        contact2.setHomeAddress("taiyuan");
-        contact2.setVpmn("655555");
-        
-        list.add(contact);
-        list.add(contact2);
-        
-        return list;
     }
 }
--- src/test/java/com/baldurtech/contact/ContactControllerTest.java
+++ src/test/java/com/baldurtech/contact/ContactControllerTest.java
@@ -3,16 +3,55 @@ package com.baldurtech.contact;
 import org.junit.Test;
 import com.baldurtech.config.WebAppConfigurationAware;
 
+import java.util.List;
+import java.util.ArrayList;
+
+import org.mockito.Mock;
+import static org.mockito.Mockito.when;
+import static org.hamcrest.Matchers.allOf;
+import static org.hamcrest.Matchers.containsString;
+
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 public class ContactControllerTest extends WebAppConfigurationAware {
 
+    @Mock
+    ContactService contactService;
+    
+    public void setup() {
+        List<Contact> list = new ArrayList<Contact>();
+        Contact contact = new Contact();       
+        contact.setName("shihang");
+        contact.setMobile("15235432994");
+        contact.setHomeAddress("shanxi");
+        contact.setVpmn("652994");
+         
+        Contact contact2 = new Contact();
+        contact2.setName("xiaobai");
+        contact2.setMobile("18235433333");
+        contact2.setHomeAddress("taiyuan");
+        contact2.setVpmn("655555");
+         
+        list.add(contact);
+        list.add(contact2);
+        
+        when(contactService.getList()).thenReturn(list);
+    }
+    
    @Test
     public void 当URI为contact_list时应该由ContactController处理() throws Exception {
         mockMvc.perform(get("/contact/list"))
                .andExpect(view().name("contact/list"))
-               .andExpect(model().attributeExists("list"));
+               .andExpect(model().attributeExists("list"))
+               .andExpect(content().string(
+                        allOf(
+                                containsString("<title>contactList</title>"),
+                                containsString("<td>shihang</td>"),
+                                containsString("<td>15235432994</td>"),
+                                containsString("<td>shanxi</td>"),
+                                containsString("<td>652994</td>")
+                        )));
 
     }
 
