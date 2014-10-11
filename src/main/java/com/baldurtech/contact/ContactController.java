23fd8eb055b927921531de36ede1f0a1c9d65aea
baijiayong <jiayongbai@gmail.com>
2014-10-11 13:58:40 +0800

    通过controller给页面传递属性list

--- src/main/java/com/baldurtech/contact/ContactController.java
+++ src/main/java/com/baldurtech/contact/ContactController.java
@@ -4,12 +4,19 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import org.springframework.ui.Model;
+
+import java.util.List;
+import java.util.ArrayList;
+
 @Controller
 @RequestMapping("contact")
 public class ContactController {
 
     @RequestMapping(value = "/list", method = RequestMethod.GET)
-    public String list() {
+    public String list(Model model) {
+        List<Contact> list = new ArrayList<Contact>();
+        model.addAttribute("list", list);
         return "contact/list";
     }
 }
--- src/test/java/com/baldurtech/contact/ContactControllerTest.java
+++ src/test/java/com/baldurtech/contact/ContactControllerTest.java
@@ -7,10 +7,12 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 public class ContactControllerTest extends WebAppConfigurationAware {
-    @Test
+
+   @Test
     public void 当URI为contact_list时应该由ContactController处理() throws Exception {
         mockMvc.perform(get("/contact/list"))
-               .andExpect(view().name("contact/list"));
+               .andExpect(view().name("contact/list"))
+               .andExpect(model().attributeExists("list"));
 
     }
 
