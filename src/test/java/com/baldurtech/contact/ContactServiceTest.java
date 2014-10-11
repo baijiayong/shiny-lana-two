bee39682b4fab6cacd8538201b2945eaff8616c7
baijiayong <jiayongbai@gmail.com>
2014-10-11 09:18:33 +0800

    添加了contactService的测试，并添加了contactRepository

--- src/test/java/com/baldurtech/contact/ContactServiceTest.java
+++ src/test/java/com/baldurtech/contact/ContactServiceTest.java
@@ -1,7 +1,32 @@
 package com.baldurtech.contact;
 
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.Ignore;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import static org.mockito.Mockito.verify;
+import org.mockito.MockitoAnnotations;
+import static org.mockito.Mockito.when;
+
+import java.util.List;
+import java.util.ArrayList;
+
 public class ContactServiceTest {
+    @InjectMocks
+    ContactService contactService;
+    
+    @Mock
+    ContactRepository contactRepository;
+    
+    @Before
+    public void initMocks() {
+        MockitoAnnotations.initMocks(this);
+    }
+    
+    @Test
     public void thatwillGetContactList() {
-        
+        contactService.getContactList();
+        verify(contactRepository).findAllContacts();
     }
 }
