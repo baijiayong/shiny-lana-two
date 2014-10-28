ea1a5c83e9e5fcc1a5a055c740f5e6771f4101cd
baijiayong <jiayongbai@gmail.com>
2014-10-28 11:46:12 +0800

    添加了对Contact的name的校验测试

---
+++ src/test/java/com/baldurtech/contact/ContactValidationTest.java
@@ -0,0 +1,32 @@
+package com.baldurtech.contact;
+
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import java.util.Set;
+
+import javax.validation.ConstraintViolation;
+import javax.validation.ValidatorFactory;
+import javax.validation.Validator;
+import javax.validation.Validation;
+
+import static org.junit.Assert.assertEquals;
+
+public class ContactValidationTest {
+    private static Validator validator;
+    
+    @BeforeClass
+    public static void setup() {
+        ValidatorFactory factory = Validation.buildDefaultValidatorFactory();
+        validator = factory.getValidator();
+    }
+    
+    @Test
+    public void nameIsNull() {
+        Contact contact = new Contact();
+        contact.setName(null);
+        
+        Set<ConstraintViolation<Contact>> constraintViolations = validator.validate(contact);
+        assertEquals("不能为空", constraintViolations.iterator().next().getMessage());
+    }
+}
