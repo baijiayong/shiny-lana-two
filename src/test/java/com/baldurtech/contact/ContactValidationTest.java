b5e71ea3209a05df8af97342b03f27c2ce0661dc
baijiayong <jiayongbai@gmail.com>
2014-10-28 11:59:40 +0800

    重构了ContactValidationTest

--- src/test/java/com/baldurtech/contact/ContactValidationTest.java
+++ src/test/java/com/baldurtech/contact/ContactValidationTest.java
@@ -14,28 +14,30 @@ import static org.junit.Assert.assertEquals;
 
 public class ContactValidationTest {
     private static Validator validator;
+    private static Contact contact;
+    private static Set<ConstraintViolation<Contact>> constraintViolations;
     
     @BeforeClass
     public static void setup() {
         ValidatorFactory factory = Validation.buildDefaultValidatorFactory();
         validator = factory.getValidator();
+        contact = new Contact();       
     }
     
     @Test
     public void nameIsNull() {
-        Contact contact = new Contact();
         contact.setName(null);
         
-        Set<ConstraintViolation<Contact>> constraintViolations = validator.validate(contact);
+        constraintViolations = validator.validate(contact);
         assertEquals("不能为空", constraintViolations.iterator().next().getMessage());
     }
     
     @Test
     public void nameIsBlank() {
-        Contact contact = new Contact();
         contact.setName("   ");
         
-        Set<ConstraintViolation<Contact>> constraintViolations = validator.validate(contact);
+        constraintViolations = validator.validate(contact);
         assertEquals("不能为空", constraintViolations.iterator().next().getMessage());   
     }
+    
 }
