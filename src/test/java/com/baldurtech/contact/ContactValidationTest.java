a529bae2b080fdcc09fc7cf4b68f4999e52c72f1
baijiayong <jiayongbai@gmail.com>
2014-10-29 09:45:55 +0800

    添加了jobLevel为null的测试

--- src/test/java/com/baldurtech/contact/ContactValidationTest.java
+++ src/test/java/com/baldurtech/contact/ContactValidationTest.java
@@ -399,5 +399,23 @@ public class ContactValidationTest {
         assertEquals(1, constraintViolations.size());
         assertEquals("不能为空", constraintViolations.iterator().next().getMessage());
     }
+        
+    @Test
+    public void jobLevelIsNull() {
+        Contact contact = new Contact();
+        contact.setName("XiaoBai");
+        contact.setMobile("18222222222");
+        contact.setVpmn("62222");
+        contact.setEmail("a@a.com");
+        contact.setHomeAddress("TaiYuan");
+        contact.setOfficeAddress("BeiZhang");
+        contact.setJob("HR");
+        contact.setJobLevel(null);
+        contact.setMemo("memo");
+
+        Set<ConstraintViolation<Contact>> constraintViolations = validator.validate(contact);
+        assertEquals(1, constraintViolations.size());
+        assertEquals("不能为null", constraintViolations.iterator().next().getMessage());
+    }
 }
 
