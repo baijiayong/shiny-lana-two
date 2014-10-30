bbf8a4077fa5e6cd8ea2c9a0f4d058f0bc408f78
baijiayong <jiayongbai@gmail.com>
2014-10-30 12:40:08 +0800

    添加了contactService，及其测试

--- src/main/java/com/baldurtech/contact/ContactController.java
+++ src/main/java/com/baldurtech/contact/ContactController.java
@@ -3,6 +3,7 @@ package com.baldurtech.contact;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.beans.factory.annotation.Autowired;
 
 import org.springframework.ui.Model;
 
@@ -12,29 +13,16 @@ import java.util.ArrayList;
 @Controller
 @RequestMapping("contact")
 public class ContactController {
+    ContactService contactService;
+    
+    @Autowired
+    public ContactController(ContactService contactService) {
+        this.contactService = contactService;
+    }
+
     @RequestMapping(value = "/list", method = RequestMethod.GET)
     public String list(Model model) {
-        model.addAttribute("contactList", findAll());
+        model.addAttribute("contactList", contactService.list());
         return "contact/list";
     }
-    
-    public List<Contact> findAll() {
-        List<Contact> contactList = new ArrayList<Contact>();
-        Contact contact1 = new Contact();
-        contact1.setName("Shi Hang");
-        contact1.setMobile("18233333333");
-        contact1.setVpmn("6333");
-        contact1.setHomeAddress("TaiYuan");
-        
-        Contact contact2 = new Contact();
-        contact2.setName("Xiao Bai");
-        contact2.setMobile("18233333333");
-        contact2.setVpmn("6333");
-        contact2.setHomeAddress("TaiYuan");
-        
-        contactList.add(contact1);
-        contactList.add(contact2);
-        
-        return contactList;
-    }    
 }
--- src/test/java/com/baldurtech/contact/ContactControllerTest.java
+++ src/test/java/com/baldurtech/contact/ContactControllerTest.java
@@ -1,17 +1,45 @@
 package com.baldurtech.contact;
 
 import org.junit.Test;
+import org.junit.Before;
+
+import org.mockito.MockitoAnnotations;
+import org.mockito.Mock;
+import org.mockito.InjectMocks;
+import static org.mockito.Mockito.verify;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
+import org.springframework.ui.Model;
+
 import com.baldurtech.config.WebAppConfigurationAware;
 
 public class ContactControllerTest extends WebAppConfigurationAware {
+    @Mock
+    Model model;
+    
+    @Mock
+    ContactService contactService;
+    
+    @InjectMocks
+    ContactController contactController;
+    
+    @Before
+    public void setup() {
+        MockitoAnnotations.initMocks(this);
+    }
+   
     @Test
     public void 当url为contact_list时应该访问list页面() throws Exception {
         mockMvc.perform(get("/contact/list"))
             .andExpect(model().attributeExists("contactList"))
             .andExpect(view().name("contact/list"));
+    }
+    
+    @Test
+    public void 在ContactController中的list方法中调用ContactService中的list方法() {
+        contactController.list(model);
+        verify(contactService).list();
     }
 }
