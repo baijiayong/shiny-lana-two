1409a8ba9e0f03ef70b3d7823bf7f3754ee05477
shihang <study1228.sh@gmail.com>
2014-10-30 15:24:14 +0800

    添加数据库连接的代码并且添加查询contactList

--- src/main/java/com/baldurtech/contact/ContactRepository.java
+++ src/main/java/com/baldurtech/contact/ContactRepository.java
@@ -4,25 +4,18 @@ import java.util.List;
 import java.util.ArrayList;
 import org.springframework.stereotype.Repository;
 
+import javax.persistence.*;
+
 @Repository
 public class ContactRepository {
+    @PersistenceContext
+    EntityManager entityManager;
+
     public List<Contact> findAll() {
-        List<Contact> contactList = new ArrayList<Contact>();
-        Contact contact1 = new Contact();
-        contact1.setName("Shi Hang");
-        contact1.setMobile("18233333333");
-        contact1.setVpmn("6333");
-        contact1.setHomeAddress("TaiYuan");
-        
-        Contact contact2 = new Contact();
-        contact2.setName("Xiao Bai");
-        contact2.setMobile("18233333333");
-        contact2.setVpmn("6333");
-        contact2.setHomeAddress("TaiYuan");
-        
-        contactList.add(contact1);
-        contactList.add(contact2);
-        
-        return contactList;
+        try {       
+            return entityManager.createNamedQuery(Contact.FIND_ALL, Contact.class).getResultList();
+        } catch(PersistenceException pe) {
+            return null;
+        }
     }    
 }
