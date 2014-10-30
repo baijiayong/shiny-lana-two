f382f46d0cba116ac86dbed12b942b1c696824b5
shihang <study1228.sh@gmail.com>
2014-10-30 15:45:58 +0800

    添加了show的测试，并且当URL为contact/show时，访问指定页面

--- src/test/java/com/baldurtech/contact/ContactControllerTest.java
+++ src/test/java/com/baldurtech/contact/ContactControllerTest.java
@@ -16,6 +16,7 @@ import org.springframework.ui.Model;
 import com.baldurtech.config.WebAppConfigurationAware;
 
 public class ContactControllerTest extends WebAppConfigurationAware {
+    private Long CONTACT_ID = 3L;
     @Mock
     Model model;
     
@@ -31,15 +32,22 @@ public class ContactControllerTest extends WebAppConfigurationAware {
     }
    
     @Test
-    public void 当url为contact_list时应该访问list页面() throws Exception {
+    public void 当URL为contact_list时应该访问list页面() throws Exception {
         mockMvc.perform(get("/contact/list"))
-            .andExpect(model().attributeExists("contactList"))
-            .andExpect(view().name("contact/list"));
+               .andExpect(model().attributeExists("contactList"))
+               .andExpect(view().name("contact/list"));
     }
     
     @Test
     public void 在ContactController中的list方法中调用ContactService中的list方法() {
         contactController.list(model);
         verify(contactService).list();
+    }
+    
+    @Test
+    public void 当URL为contact_show时应该访问show页面() throws Exception{
+        mockMvc.perform(get("/contact/show")
+                        .param("id", String.valueOf(CONTACT_ID)))
+               .andExpect(view().name("contact/show"));
     }
 }
